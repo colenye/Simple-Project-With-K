05d6042ce0f439b013a41e7f9113898321809882
Sam Kuriakose <sam_kuriakose@Sams-MacBook-Pro.local>
2026-02-11 14:10:40 -0500

    calc

--- calc.py
+++ calc.py
@@ -2,7 +2,7 @@ while (True):
     num_1 = float(input('Enter the first number:'))
     num_2 = float(input('Enter the second number:'))
     sign = input('Enter the operator: ')
-
+    
     if sign == '+':
         sum = float(num_1 + num_2)
         print(sum)
@@ -12,9 +12,11 @@ while (True):
     elif sign == '/':
         quotient = float(num_1 / num_2)
         print(quotient)
-    else sign == '-':
+    elif sign == '-':
         diff = float(num_1 - num_2)
         print(diff)
+    else:
+        print("Invalid input.")
 
 
 
